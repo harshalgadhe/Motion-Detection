c5b5168e01022010b80e86d378e3cc80aa6b0ad7
Harshal Gadhe <51368042+harshalgadhe@users.noreply.github.com>
2020-12-02 19:27:31 +0530

    Add files via upload

---
+++ motion_detector.py
@@ -0,0 +1,40 @@
+import cv2 as cv
+from datetime import datetime
+import pandas
+import time
+
+first_frame=None
+video=cv.VideoCapture(0,cv.CAP_DSHOW)
+
+while True:
+    check,frame=video.read()
+    gray=cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+    gray=cv.GaussianBlur(gray,(21,21),0)
+
+    if first_frame is None:
+        first_frame=gray
+        continue
+
+    delta_frame=cv.absdiff(first_frame,gray)
+    thresh_delta=cv.threshold(delta_frame,30,255,cv.THRESH_BINARY)[1]
+    thresh_delta=cv.dilate(thresh_delta,None,iterations=0)
+    cnts ,_ =cv.findContours(thresh_delta.copy(),cv.RETR_EXTERNAL,cv.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv.contourArea(contour)<10000:
+            continue
+        (x,y,w,h)=cv.boundingRect(contour)
+        cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
+
+    cv.imshow('frame',frame)
+    cv.imshow('capturing',gray)
+    cv.imshow('delta',delta_frame)
+    cv.imshow('thresh',thresh_delta)
+
+    key=cv.waitKey(1)
+
+    if key==ord('q'):
+        break
+
+video.release()
+cv.destroyAllWindows()
